794782b6ee189a5b90ee6d19c9378d260df885f5
mars-86 <hrslash86@gmail.com>
2024-06-08 13:31:17 -0300

    feat: added signature functions to alloc and release dataset

--- src/dataset.h
+++ src/dataset.h
@@ -1,6 +1,7 @@
 #ifndef __PICTURE_TRANSFER_PROTOCOL_DATASET_INCLUDED_H__
 #define __PICTURE_TRANSFER_PROTOCOL_DATASET_INCLUDED_H__
 
+#include <cstdint>
 #include <stdint.h>
 
 #define PTP_DEVICE_FUNCTIONAL_MODE_STANDARD 0x0000
@@ -11,17 +12,21 @@ struct device_info {
     uint16_t StandardVersion;
     uint32_t VendorExtensionID;
     uint16_t VendorExtensionVersion;
-    char* VendorExtensionDesc;
+    uint16_t* VendorExtensionDesc; // each char is 2 bytes
     uint16_t FunctionalMode;
     uint16_t* OperationsSupported;
     uint16_t* EventsSupported;
     uint16_t* DevicePropertiesSupported;
     uint8_t* CaptureFormats;
     uint32_t* ImageFormats;
-    char* Manufacturer;
-    char* Model;
-    char* DeviceVersion;
-    char* SerialNumber;
+    uint16_t* Manufacturer; // each char is 2 bytes
+    uint16_t* Model; // each char is 2 bytes
+    uint16_t* DeviceVersion; // each char is 2 bytes
+    uint16_t* SerialNumber; // each char is 2 bytes
 } __attribute__((packed));
+
+device_info* alloc_device_info(const uint8_t* stream);
+
+void free_device_info(const device_info* dev_info);
 
 #endif // __PICTURE_TRANSFER_PROTOCOL_DATASET_INCLUDED_H__
