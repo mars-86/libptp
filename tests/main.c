a7de218fbb54afebb9e2635fff6bca899ac34817
mars-86 <hrslash86@gmail.com>
2024-06-07 21:20:28 -0300

    refac: device_info dataset

--- tests/main.c
+++ tests/main.c
@@ -67,6 +67,62 @@ int main(void)
     // putc(buffer[i], stdout);
     putc('\n', stdout);
 
+    for (int i = 12; i < 14; ++i)
+        printf("%.2X", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 14; i < 18; ++i)
+        printf("%.2X", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 18; i < 20; ++i)
+        printf("%.2X", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 21; i < 259; ++i)
+        printf("%c", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 259; i < 261; ++i)
+        printf("%c", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 265; i < 341; ++i)
+        printf("%.2X", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 345; i < 355; ++i)
+        printf("%.2X", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 359; i < 369; ++i)
+        printf("%.2X", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 377; i < 417; ++i)
+        printf("%.2X", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 418; i < 479; ++i)
+        printf("%c", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 479; i < 498; ++i)
+        printf("%c", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 498; i < 526; ++i)
+        printf("%c", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 527; i < 553; ++i)
+        printf("%c", buffer[i]);
+    putc('\n', stdout);
+
+    for (int i = 0; i < 300; ++i)
+        printf("%.2X", buffer[i]);
+    putc('\n', stdout);
+
     ASSERT_TEST_SUMMARY();
 
     return 0;
